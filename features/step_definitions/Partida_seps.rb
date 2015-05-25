18586d723bbfdab42e20559c27731c3532bb5fe5
lucazartu <laqcs@cin.ufpe.br>
2015-05-25 16:49:40 -0300

    Criação dos arquivos de testes de partidas

---
+++ features/step_definitions/Partida_seps.rb
@@ -0,0 +1,29 @@
+Given(/^estou visualizando a lista de partidas$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^eu clico no campo "data"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^serão exibidas as partidas ordenadas pela sua data$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+    
+Given(/^estou visualizando uma partida$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^eu clico na opção "alterar partida"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^uma nova pagina é aberta$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^é exibido um formulário para alterar as informações da partidas$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
