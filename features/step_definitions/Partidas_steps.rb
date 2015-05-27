68ff7455dc577e50edb29c7e41764b6b8e815751
lucazartu <laqcs@cin.ufpe.br>
2015-05-26 23:40:16 -0300

    [#95492398] Implementação, manutenção e execução de testes do cenário de GUI de adicionar Partida

--- features/step_definitions/Partidas_steps.rb
+++ features/step_definitions/Partidas_steps.rb
@@ -16,3 +16,30 @@ Then(/^apartida com data "([^"]*)",  com nome "([^"]*)", com tag "([^
   partida = Partida.new("data", "nome", "tag")
   partida.adicionarPartida(arg1, arg2, arg3)
 end
+
+Given(/^estou na pagina principal do time$/) do
+  visit LoginPage
+  fill_in "Login", :with => @supervisor.login
+  fill_in "Password", :with => @supervisor.password
+  visit GerenciamentoPartidasPage
+  click_button "Partidas"
+  visit PartidasPage
+end
+
+When(/^eu clico na opcao "Adicionar Partida"$/) do
+  visit PartidasPage
+  click_button "Adicionar Partida"
+end
+
+Then(/^a pagina de "Nova Partida" e exibida$/) do
+  visit PartidasPage
+  click_button "Adicionar Partida"
+  visit NovaPartidaPage
+end
+
+Then(/^eu posso preencher o formulario de cadastro\.$/) do
+  visit NovaPartidaPage
+  fill_in 'Data', :with => 'Data'
+  fill_in 'Nome', :with => 'Nome'
+  fill_in 'Tag', :with => 'Tag'
+end
