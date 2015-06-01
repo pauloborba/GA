1fa4e1dbe4b443ce083c0612e59981479ddae9e3
marciat7 <mctc@cin.ufpe.br>
2015-06-01 18:27:07 -0300

    Classes de teste

---
+++ features/step_definitions/contrato_steps.rb
@@ -0,0 +1,20 @@
+# CONTROLLER
+# scenario: Adicionar cláusula “Saldo de Gols”
+
+Given(/^que existe um contrato entre contratante "Náutico" e contratada "Miley C
+yrus"$/) do
+  puts "oi"
+end
+
+Given(/^o contrato não possui a cláusula "Saldo de Gols"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^eu adiciono a cláusula "Saldo de Gols"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^a cláusula "Saldo de Gols" é adicionada ao contrato entre contratante "Ná
+utico" e contratada "Miley Cyrus"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
