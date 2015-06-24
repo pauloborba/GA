116811e6594551e9c50b829cf38f3f2ff0f43067
Igor Lira <igorlira@live.com>
2015-06-24 15:04:06 -0300

    Validates contract start and end dates presence

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -4,6 +4,8 @@ class Contrato < ActiveRecord::Base
   validate :end_date_must_be_greater_than_start_date
   validates :valor, numericality: { :greater_than_or_equal_to => 0 }
   validates :atleta, presence: true
+  validates :data_Inicio, presence: true
+  validates :data_Termino, presence: true
 
   def end_date_must_be_greater_than_start_date
   	errors.add(:data_Termino, "The end date must be greater than the start date") if self.data_Termino <= self.data_Inicio
