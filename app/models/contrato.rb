d4478951dad65b07a742aa8c0cda2e95d963d73c
Igor Lira <igorlira@live.com>
2015-06-24 13:58:57 -0300

    Validate contract value before saving

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -2,6 +2,7 @@ class Contrato < ActiveRecord::Base
   belongs_to :atleta
   has_many :clausulas
   validate :end_date_must_be_greater_than_start_date
+  validates :valor, numericality: { :greater_than_or_equal_to => 0 }
 
   def end_date_must_be_greater_than_start_date
   	errors.add(:data_Termino, "The end date must be greater than the start date") if self.data_Termino <= self.data_Inicio
