ebd131612d04509272de6d2b02385d39b44e9170
Igor Lira <igorlira@live.com>
2015-06-24 15:05:51 -0300

    Fix contract model validation

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -8,6 +8,6 @@ class Contrato < ActiveRecord::Base
   validates :data_Termino, presence: true
 
   def end_date_must_be_greater_than_start_date
-  	errors.add(:data_Termino, "The end date must be greater than the start date") if self.data_Termino <= self.data_Inicio
+  	errors.add(:data_Termino, "The end date must be greater than the start date") if self.data_Inicio != nil and self.data_Termino != nil and self.data_Termino <= self.data_Inicio
   end
 end
