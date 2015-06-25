74bd4074df587a0b1df20d5a63ae625bf76fd193
JoaoRicardoSantos <jrs4@cin.ufpe.br>
2015-06-25 18:58:32 -0300

    [#97855550]Implementar testes para o cenários de Controle da criação de partidas

--- features/step_definitions/Partida_steps.rb
+++ features/step_definitions/Partida_steps.rb
@@ -1,33 +1,69 @@
 # -*- encoding : utf-8 -*-
-Given(/^que nao existe uma partida com data “(\d+)\/(\d+)\/(\d+)”, com nome “Nautico VS Sport”, com tag “Serie A”\.$/) do |data, nome, tag|
-    @result = Partida.find_partida(data, nome, tag)
-    @result == false
+# begin @JoaoSantos
+
+Given(/^que nao existe uma partida com data “([^"]*)”, com nome “([^"]*)”, com tag “([^"]*)”\.$/) do |data, nome, tag|
+    @result = find_or_include_partida(nome, data, tag)
+
+    expect(@result).to eq(nil)
+    #if @result  == nil
+    #  puts "Partida não existente."
+    #else
+    #  puts @result.nome
+    #  puts @result.data
+    #  puts @result.tag
+    #end
 end
 
-When(/^eu crio uma partida com data “(\d+)\/(\d+)\/(\d+)”,  com nome “Nautico VS Sport”, com tag “Serie A”\.$/) do |data, nome, tag|
-    @new_partida = Partida.create_partida(data, nome, tag)
-    @new_partida != nil
+When(/^eu crio uma partida com data “([^"]*)”,  com nome “([^"]*)”, com tag “([^"]*)”\.$/) do |data, nome, tag|
+    @new_partida = nil
+    if @result == nil
+      @new_partida = Partida.new({data: data, nome: nome, tag: tag})
+      @new_partida.save()
+    end
+
+    @new_partida.should_not eq(nil)
+    #if @new_partida  == nil
+    #  puts "Partida não adicionada."
+    #else
+    #  puts @new_partida.nome
+    #  puts @new_partida.data
+    #  puts @new_partida.tag
+    #end
 end
 
-Then(/^a partida com data “(\d+)\/(\d+)\/(\d+)”, com nome “Nautico VS Sport”, com tag “Serie A” eh salva no sistema\.$/) do |data, nome, tag|
-    @current_partida = Partida.find_partida(data, nome, tag)
-    @current_partida == true
+Then(/^a partida com data “([^"]*)”, com nome “([^"]*)”, com tag “([^"]*)” eh salva no sistema\.$/) do |data, nome, tag|
+    @current_partida = find_or_include_partida(nome, data, tag)
+
+    @current_partida.should_not eq(nil)
+
+    #if @current_partida  == nil
+    #  puts "Partida não salva."
+    #else
+    #  puts "Partida salva no sistema"
+    #end
 end
 
 
-Given(/^que eu tenha no sistema a partida com data "(\d+)\/(\d+)\/(\d+)”, com nome “Nautico VS Sport”, com tag “Serie A”\.$/) do |data, nome, tag|
-    @current_partida = Partida.find_partida(data, nome, tag)
-    @current_partida == true
+def find_or_include_partida(nome, data, tag, list = nil)
+  result = nil
+  current_list = Partida.where(nome: nome, tag: tag)
+  if list != nil
+    current_list = list.where(nome: nome, tag: tag)
+  end
+  current_list.find_each do |partida|
+    data_corrente = partida.data
+
+    if (data_corrente.day.to_i == data[0..2].to_i) and (data_corrente.month.to_i == data[3..5].to_i) and (data_corrente.year.to_i == data[6..10].to_i)
+      result = partida
+      break
+    end
+  end
+  return result
 end
 
-When(/^eu vejo a lista de partidas existentes\.$/) do
-    @current_list_partidas = Partida.listPartidas()
-end
 
-Then(/^minha lista de paridas contem a partida com data "(\d+)\/(\d+)\/(\d+)”, com nome “Nautico VS Sport”, com tag “Serie A”\.$/) do |data, nome, tag|
-    @existe = @current_list_partidas.include?(Partida.find_partida(data, nome, tag))
-    @existe == true
-end
+
+# end @JoaoSantos
 
 Given(/^que existe uma partida com data "([^"]*)", com nome "([^"]*)", com tag "([^"]*)"$/) do |arg1, arg2, arg3|
   partida = Partida.new()
