948e01d77d74b7fee2fba80d54855c7e019fef5a
Emanuel Silva <emanuel.fisilva@gmail.com>
2016-05-04 18:30:05 -0300

    Criando os steps de AdicionarAtletas
    Criando os steps dos dois scenarios de Adicionar Atletas

---
+++ test/cucumber/steps/adicionarAtletasSteps.groovy
@@ -0,0 +1,35 @@
+Given(~'^Estou no menu de Atletas$') { ->
+    at AtletasPage
+}
+
+And(~'^O atleta de CPF "([^"]*)" não esta na lista de atletas$') {String cpf->
+    at AtletasPage
+    assert AtletasPage.selectAtleta(cpf) == false
+}
+
+When(~'^Eu tento cadastrar o atleta "([^"]*)" com o CPF "([^"]*)"$') { String nome, cpf ->
+    at AtletasPage
+    AtletasPage.fillNome(nome)
+    AtletasPage.fillCpf(cpf)
+}
+
+Then(~'^Eu posso ver uma mensagem de sucesso e o nome "([^"]*)" e CPF "([^"]*)"$') { String nome, cpf
+    at AtletasPage
+    at ConfirmationPage
+    assert ConfirmationPage.selectNome(nome) == true
+    assert ConfirmationPage.selectCpf(cpf) == true
+}
+
+Given(~'^O atleta de CPF "([^"]*)" não esta cadastrado no sistema$') { String cpf ->
+    atleta = Atletas.findByCPF(cpf)
+    assert atleta == null
+}
+
+When(~'^Eu adiciono o atleta de CPF"([^"]*)"$') { String cpf ->
+    Atletas.addAtleta(null, cpf, null, null)
+}
+
+Then(~'^O sistema adiciona o atleta com sucesso$') { String cpf ->
+    atleta = Atletas.findByCPF(cpf)
+    assert atleta != null
+}
