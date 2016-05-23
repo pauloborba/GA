92fb2b8156ac89bb1ed8f6a77a74fcba9284ad3d
Emanuel Silva <emanuel.fisilva@gmail.com>
2016-05-23 20:59:22 -0300

    Update adicionarAtletasSteps.groovy
    Ajustando corpo dos Steps e espaços entre parâmetros

--- test/cucumber/steps/adicionarAtletasSteps.groovy
+++ test/cucumber/steps/adicionarAtletasSteps.groovy
@@ -15,12 +15,12 @@ Given(~'^O atleta de CPF "([^"]*)" não esta cadastrado no sistema$') { String c
     assert Atleta.findByCPF(cpf) == null
 }
 
-When(~'^Eu cadastro o atleta de CPF"([^"]*)"$') { String cpf ->
+When(~'^Eu cadastro o atleta de CPF "([^"]*)"$') { String cpf ->
     def controlador = new AtletaController()
     cadastrarAtleta(cpf,controlador)
 }
 
-Then(~'^O sistema adiciona o atleta com sucesso$') { String cpf ->
+Then(~'^O sistema adiciona o atleta de CPF "([^"]*)" com sucesso$') { String cpf ->
     assert Atleta.findByCPF(cpf) != null
 }
 
@@ -30,23 +30,23 @@ def cadastrarAtleta(String cpf, AtletaController controlador) {
     controlador.response.reset()
 }
 
-Given(~/^Estou no menu de Atletas$/) { ->
+Given(~'^Estou no menu de Atletas$') { ->
     to AtletasPage
     at AtletasPage
 }
 
-And(~/^O atleta de CPF "([^"]*)" não esta na lista de atletas$/) {String nome, cpf->
+And(~'^O atleta de CPF "([^"]*)" e nome "([^"]*)" não esta na lista de atletas$') {String nome, cpf->
     at AtletasPage
     assert !(page.atletaNaLista(nome, cpf))
 }
 
-When(~/^Eu tento cadastrar o atleta "([^"]*)" com o CPF "([^"]*)"$/) { String nome, cpf ->
+When(~'^Eu tento cadastrar o atleta "([^"]*)" com o CPF "([^"]*)"$') { String nome, cpf ->
     to CreateAtleta
     at CreateAtleta
     page.cadastrarAtleta(nome, cpf)
 }
 
-Then(~/^Eu posso ver a tela de visualização de atleta e o nome "([^"]*)" e CPF "([^"]*)"$/) { String nome, cpf ->
+Then(~'^Eu posso ver a tela de visualização de Atleta e o nome "([^"]*)" e CPF "([^"]*)"$') { String nome, cpf ->
     at VerAtleta
     assert page.temNome(nome) == true
     assert page.temCpf(cpf) == true
