3a7d85f634598a436ddd56b87a782fb5de9fdeb8
Emanuel Silva <emanuel.fisilva@gmail.com>
2016-05-23 21:52:19 -0300

    Update Atleta.groovy
    Adicionando o atributo dataNascimento
    Adicionando os constraints dos atributos

--- grails-app/domain/ga/Atleta.groovy
+++ grails-app/domain/ga/Atleta.groovy
@@ -3,7 +3,10 @@ package ga
 class Atleta {
     String nome
     String cpf
+    Date dataNascimento
 
     static constraints = {
+        cpf blank: false
+        dataNascimento nullable: true
     }
 }
