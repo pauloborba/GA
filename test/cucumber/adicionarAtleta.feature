8ed8c4ed1d492be15d6d932502bca84fa4e083ed
silvaemanuel <efis@cin.ufpe.br>
2016-06-01 22:07:28 -0300

    Adicionando meus arquivos de entregas passadas

--- test/cucumber/adicionarAtleta.feature
+++ test/cucumber/adicionarAtleta.feature
@@ -15,4 +15,13 @@ Feature: Adicionar Atleta
     And Tento cadastrar um novo atleta "Ryan" com o CPF "863923834-34"
     Then Vejo uma mensagem erro "Este CPF já está cadastrado"
 
+  Scenario: Adicionar Atleta no sistema com sucesso
+    Given O atleta de CPF "01234567890" não esta cadastrado no sistema
+    When Eu cadastro o atleta de CPF "01234567890"
+    Then O sistema adiciona o atleta de CPF "01234567890" com sucesso
 
+  Scenario: Adicionar Atleta no sistema com sucesso web
+    Given Estou no menu de Atletas
+    And O atleta de CPF "01234567890" e nome "Pedro" não esta na lista de atletas
+    When Eu tento cadastrar o atleta "Pedro" com o CPF "01234567890"
+    Then Eu posso ver a tela de visualização de Atleta e o nome "Pedro" e CPF "01234567890"
--- test/cucumber/steps/adicionarAtletaSteps.groovy
+++ test/cucumber/steps/adicionarAtletaSteps.groovy
@@ -73,4 +73,45 @@ Then(~'Eu posso ver que na página de Atletas o no atleta não foi adicionado$')
 	at AtletasPage
 }
 
-//criar AtletasPage
+//Abaixo, steps de silvaemanuel
+
+Given(~'^O atleta de CPF "([^"]*)" não esta cadastrado no sistema$') { String cpf ->
+    assert Atleta.findByCPF(cpf) == null
+}
+
+When(~'^Eu cadastro o atleta de CPF "([^"]*)"$') { String cpf ->
+    def controlador = new AtletaController()
+    cadastrarAtleta(cpf,controlador)
+}
+
+Then(~'^O sistema adiciona o atleta de CPF "([^"]*)" com sucesso$') { String cpf ->
+    assert Atleta.findByCPF(cpf) != null
+}
+
+def cadastrarAtleta(String cpf, AtletaController controlador) {
+    controlador.params << [cpf: cpf, nome: "", dataNascimento: null]
+    controlador.save()
+    controlador.response.reset()
+}
+
+Given(~'^Estou no menu de Atletas$') { ->
+    to AtletasPage
+    at AtletasPage
+}
+
+And(~'^O atleta de CPF "([^"]*)" e nome "([^"]*)" não esta na lista de atletas$') {String nome, cpf->
+    at AtletasPage
+    assert !(page.atletaNaLista(nome, cpf))
+}
+
+When(~'^Eu tento cadastrar o atleta "([^"]*)" com o CPF "([^"]*)"$') { String nome, cpf ->
+    to CreateAtleta
+    at CreateAtleta
+    page.cadastrarAtleta(nome, cpf)
+}
+
+Then(~'^Eu posso ver a tela de visualização de Atleta e o nome "([^"]*)" e CPF "([^"]*)"$') { String nome, cpf ->
+    at VerAtleta
+    assert page.temNome(nome) == true
+    assert page.temCpf(cpf) == true
+}
