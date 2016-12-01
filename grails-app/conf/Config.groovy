a726d004338b5b99a02634ea61391a23f21e9948
Thomas <tafm@cin.ufpe.br>
2016-12-01 14:45:13 -0300

    [#133873987] Views e messages relacionadas ao cadastro de contrato

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -48,8 +48,8 @@ grails {
             encoding = 'UTF-8'
             htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
             codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
+                expression = 'none' // escapes values inside ${}
+                scriptlet = 'none' // escapes output from scriptlets in GSPs
                 taglib = 'none' // escapes output from taglibs
                 staticparts = 'none' // escapes output from static template parts
             }
